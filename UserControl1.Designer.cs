e030f190069f1bfbd1e4d881356fbe82e324e2d9
OctavioRode <al351788@edu.uaa.mx>
2024-11-24 23:35:13 -0600

    Agregar el proyecto

---
+++ UserControl1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Suma_de_dos_numeros
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtN1 = new System.Windows.Forms.TextBox();
+            this.TxtR = new System.Windows.Forms.TextBox();
+            this.TxtN2 = new System.Windows.Forms.TextBox();
+            this.Salir = new System.Windows.Forms.Button();
+            this.Calcular = new System.Windows.Forms.Button();
+            this.Limpiar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(207, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Primer numero";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.label2.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(355, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(277, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Suma de dos numeros ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(207, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Segundo numero";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(237, 326);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Resultado";
+            // 
+            // TxtN1
+            // 
+            this.TxtN1.Location = new System.Drawing.Point(406, 156);
+            this.TxtN1.Name = "TxtN1";
+            this.TxtN1.Size = new System.Drawing.Size(195, 26);
+            this.TxtN1.TabIndex = 5;
+            // 
+            // TxtR
+            // 
+            this.TxtR.Location = new System.Drawing.Point(406, 320);
+            this.TxtR.Name = "TxtR";
+            this.TxtR.Size = new System.Drawing.Size(195, 26);
+            this.TxtR.TabIndex = 6;
+            // 
+            // TxtN2
+            // 
+            this.TxtN2.Location = new System.Drawing.Point(406, 208);
+            this.TxtN2.Name = "TxtN2";
+            this.TxtN2.Size = new System.Drawing.Size(195, 26);
+            this.TxtN2.TabIndex = 7;
+            // 
+            // Salir
+            // 
+            this.Salir.Location = new System.Drawing.Point(687, 308);
+            this.Salir.Name = "Salir";
+            this.Salir.Size = new System.Drawing.Size(174, 41);
+            this.Salir.TabIndex = 8;
+            this.Salir.Text = "Salir";
+            this.Salir.UseVisualStyleBackColor = true;
+            this.Salir.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Calcular
+            // 
+            this.Calcular.Location = new System.Drawing.Point(687, 217);
+            this.Calcular.Name = "Calcular";
+            this.Calcular.Size = new System.Drawing.Size(174, 41);
+            this.Calcular.TabIndex = 9;
+            this.Calcular.Text = "Calcular";
+            this.Calcular.UseVisualStyleBackColor = true;
+            this.Calcular.Click += new System.EventHandler(this.Calcular_Click);
+            // 
+            // Limpiar
+            // 
+            this.Limpiar.Location = new System.Drawing.Point(687, 156);
+            this.Limpiar.Name = "Limpiar";
+            this.Limpiar.Size = new System.Drawing.Size(174, 41);
+            this.Limpiar.TabIndex = 10;
+            this.Limpiar.Text = "Limpiar";
+            this.Limpiar.UseVisualStyleBackColor = true;
+            this.Limpiar.Click += new System.EventHandler(this.Limpiar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(364, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(268, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Uriel Octavio Rodea Esquivel ";
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Limpiar);
+            this.Controls.Add(this.Calcular);
+            this.Controls.Add(this.Salir);
+            this.Controls.Add(this.TxtN2);
+            this.Controls.Add(this.TxtR);
+            this.Controls.Add(this.TxtN1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(904, 450);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtN1;
+        private System.Windows.Forms.TextBox TxtR;
+        private System.Windows.Forms.TextBox TxtN2;
+        private System.Windows.Forms.Button Salir;
+        private System.Windows.Forms.Button Calcular;
+        private System.Windows.Forms.Button Limpiar;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ UserControl1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Suma_de_dos_numeros
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.ParentForm.Close();
+        }
+
+        private void Limpiar_Click(object sender, EventArgs e)
+        {
+            TxtN1.Text = "";
+            TxtN2.Text = "";
+            TxtR.Text = "";
+        }   
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calcular_Click(object sender, EventArgs e)
+        {
+            double N1, N2, R;
+            N1=double.Parse(TxtN1.Text);
+            N2 =double.Parse(TxtN2.Text);
+            R=N1 + N2;
+            TxtR.Text=R.ToString();
+        }
+    }
+}
